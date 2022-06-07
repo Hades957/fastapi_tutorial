f324cd15fcaead36f9ef24fcf7e937cdf51048cf
zhengbifeng <zhengbifeng@bz.cn>
2022-06-07 22:33:37 +0800

    自动部署测试8

--- test.py
+++ test.py
@@ -1,20 +1,35 @@
+import datetime
+
+import requests
 from pydantic import BaseModel
 
 
-class A(BaseModel):
-  prop1: str
-  prop2: str
+# class A(BaseModel):
+#     prop1: str
+#     prop2: str
+#
+#
+# class B(BaseModel):
+#     a: A
+#
+#
+# data = {
+#     'prop1': 'some value',
+#     'prop2': 'some other value'
+# }
+#
+# b = B(a=A(**data))
+#
+# print(b)
 
+if __name__ == '__main__':
+    url = "http://support.bz.cn/pushWeChat/pushMsg"
 
-class B(BaseModel):
-  a: A
+    datas = {
 
+        'content': 'fastapi_tutorial部署完成!\n' + str(datetime.datetime.now())[0: 19],
 
-data = {
-  'prop1': 'some value',
-  'prop2': 'some other value'
-}
+        'uid': 'UID_9F1qnBI9X6Bc3sp1JdCLqaHvSQEg'
+    }
 
-b = B(a=A(**data))
-
-print(b)
+    requests.post(url, json=datas)
