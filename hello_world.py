5a8d1eeed310f70c0efaf28ead08d353eae02370
zhengbifeng <zhengbifeng@bz.cn>
2022-06-07 21:37:18 +0800

    pydantic基础

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class CityInfo(BaseModel):
+    province: str
+    country: str
+    is_affected: Optional[bool] = None  # 选填，默认None
+
+
+# 使用装饰器定义路由
+@app.get('/')
+def hello_world():
+    return {'hello': 'world'}
+
+
+@app.get('/city/{city}')
+def result(city: str, query_string: Optional[str] = None):  # city:路径参数，query_string:查询参数
+    return {'city': city, 'query_string': query_string}
+
+
+@app.put('/city/{city}')
+def result(city: str, city_info: CityInfo):
+    return {'city': city, 'country': city_info.country, 'is_affected': city_info.is_affected}
+# 启动命令：uvicorn hello_world:app --reload
